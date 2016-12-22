307e3cd2117a199fe79fef3981b49369b6f7a33b
pddivine <pddivine@gmail.com>
2016-12-21 20:35:06 -0800

    Added deployment configs

--- truffle.js
+++ truffle.js
@@ -4,5 +4,28 @@ module.exports = {
     port: 8545,
     gas: 4500000
   },
-  migrations_directory: './migrations'
+  migrations_directory: './migrations',
+  networks: {
+ "live": {
+    network_id: 42, // Ethereum public network
+    // optional config values
+    // host - defaults to "localhost"
+    // port - defaults to 8545
+    // gas
+    // gasPrice
+    // from - default address to use for any transaction Truffle makes during migrations
+  },
+  "morden": {
+    network_id: 2,        // Official Ethereum test network
+    host: "178.25.19.88", // Random IP for example purposes (do not use)
+    port: 80             
+  },
+  "staging": {
+    network_id: 1337 // custom private network
+    // use default rpc settings
+  },
+  "development": {
+    network_id: 42 //"default"
+  }
+  }
 }
